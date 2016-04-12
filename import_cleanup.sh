75db804884d4a1497bf59ebcfb1446d83b98dc90
Eugene Bujak <hmage@hmage.net>
2016-04-12 21:07:12 +0300

    Remove *.hide too

--- import_cleanup.sh
+++ import_cleanup.sh
@@ -2,6 +2,7 @@
 
 find . -name '*.new' -print0 | xargs -0 git rm -r
 find . -name '*.bak' -print0 | xargs -0 git rm -r
+find . -name '*.hide' -print0 | xargs -0 git rm -r
 find . -name '*.good' -print0 | xargs -0 git rm -r
 find . -name '*.orig' -print0 | xargs -0 git rm -r
 find . -name '*.del' -print0 | xargs -0 git rm -r
