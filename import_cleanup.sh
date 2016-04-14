43563e9246a4d647bac758f50d3898d2776e3475
Eugene Bujak <hmage@hmage.net>
2016-04-14 22:52:28 +0300

    do import cleanup before committing

--- import_cleanup.sh
+++ import_cleanup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-find . -name '*.new' -print0 | xargs -0 git rm -r
-find . -name '*.bak' -print0 | xargs -0 git rm -r
-find . -name '*.hide' -print0 | xargs -0 git rm -r
-find . -name '*.good' -print0 | xargs -0 git rm -r
-find . -name '*.orig' -print0 | xargs -0 git rm -r
-find . -name '*.del' -print0 | xargs -0 git rm -r
+find . -name '*.new' -print0 | xargs -0 rm -rvf
+find . -name '*.bak' -print0 | xargs -0 rm -rvf
+find . -name '*.hide' -print0 | xargs -0 rm -rvf
+find . -name '*.good' -print0 | xargs -0 rm -rvf
+find . -name '*.orig' -print0 | xargs -0 rm -rvf
+find . -name '*.del' -print0 | xargs -0 rm -rvf
