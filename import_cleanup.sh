8d8da92f001267894c818df18f11a1b57ce80b92
Eugene Bujak <hmage@hmage.net>
2016-04-14 22:55:17 +0300

    Update cleanup script

--- import_cleanup.sh
+++ import_cleanup.sh
@@ -5,4 +5,6 @@ find . -name '*.bak' -print0 | xargs -0 rm -rvf
 find . -name '*.hide' -print0 | xargs -0 rm -rvf
 find . -name '*.good' -print0 | xargs -0 rm -rvf
 find . -name '*.orig' -print0 | xargs -0 rm -rvf
+find . -name '*.old' -print0 | xargs -0 rm -rvf
+find . -name '*.fail' -print0 | xargs -0 rm -rvf
 find . -name '*.del' -print0 | xargs -0 rm -rvf
